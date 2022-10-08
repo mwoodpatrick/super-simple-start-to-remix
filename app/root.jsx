049c18954bbfacdd468df9e597eafcba5bbb968b
mwoodpatrick <mwoodpatrick@gmail.com>
2022-10-08 12:04:20 -0700

    first version

---
+++ app/root.jsx
@@ -0,0 +1,16 @@
+import * as React from "react";
+
+export default function App() {
+  const [count, setCount] = React.useState(0);
+  return (
+    <html>
+      <head>
+        <title>My First Remix App</title>
+      </head>
+      <body>
+        <p>This is a remix app. Hooray!</p>
+        <button onClick={() => setCount((c) => c + 1)}>{count}</button>
+      </body>
+    </html>
+  );
+}
